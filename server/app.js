ec4197f4fee005dade0bbce476b0697fd135b7f2
James Oh <jo9347@cs.rit.edu>
2023-06-17 19:36:50 -0700

    Fix slack api

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { WebClient } = require('@slack/web-api');
+// Read a token from the environment variables
+const token = process.env.SLACK_TOKEN;
+// Initialize
+const web = new WebClient(token);
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
