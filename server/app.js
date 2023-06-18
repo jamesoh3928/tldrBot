9f518f905148c2ccc208a9d318c585a25d648a56
Jack Oh <jo8842@g.rit.edu>
2023-06-17 21:12:33 -0700

    Retrieve conversation history from specific channel after certain time

--- server/app.js
+++ server/app.js
@@ -1,34 +1,77 @@
 // Load environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Set up server
 const express = require("express");
 const app = express();
 const port = 3000;
 
-const { WebClient } = require('@slack/web-api');
+const { WebClient } = require("@slack/web-api");
 // Read a token from the environment variables
 const token = process.env.TURBOSUM_SLACK_TOKEN;
 // Initialize
-const web = new WebClient(token);
+const client = new WebClient(token);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-  // Given some known conversation ID (representing a public channel, private channel, DM or group DM)
-  const conversationId = 'C05BWSD49MG';
+  // Given some known conversation ID ask_director id
+  const conversationId = "C05BWSD49MG";
 
-  (async () => {
-    // Post a message to the channel, and await the result.
-    // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-    const result = await web.chat.postMessage({
-      text: 'From the other side of apocalypse',
-      channel: conversationId,
-    });
+  // TEST for sending chat to ask_director channel
+  // (async () => {
+  //   // Post a message to the channel, and await the result.
+  //   // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+  //   const result = await client.chat.postMessage({
+  //     text: "From the other side of apocalypse",
+  //     channel: conversationId,
+  //   });
 
-    // The result contains an identifier for the message, `ts`.
-    console.log(`Successfully send message ${result.ts} in conversation ${conversationId}`);
-    console.log(result);
-  })();
+  //   // The result contains an identifier for the message, `ts`.
+  //   console.log(
+  //     `Successfully send message ${result.ts} in conversation ${conversationId}`
+  //   );
+  //   console.log(result);
+  // })();
+});
+
+app.get("/summary", (req, res) => {
+  console.log("Retrieving conversation history...");
+
+  (async (res) => {
+    // Store conversation history
+    let conversationHistory;
+    // ID of channel you watch to fetch the history for random
+    let channelId = "C05C9FUH8M7";
+
+    try {
+      console.log("In try");
+
+      // Get date
+      dateTimeString = "06/17/2023 16:00:00";
+      const dateTime = Math.floor(
+        new Date(dateTimeString).getTime() / 1000
+      ).toLocaleString();
+
+      // Call the conversations.history method using WebClient
+      const result = await client.conversations.history({
+        channel: channelId,
+        oldest: dateTime,
+      });
+      console.log("retrieved result");
+
+      conversationHistory = result.messages;
+
+      // Print results
+      console.log(
+        conversationHistory.length + " messages found in " + channelId
+      );
+
+      // Send result
+      res.send(JSON.stringify(conversationHistory));
+    } catch (error) {
+      console.error(error);
+    }
+  })(res);
 });
 
 app.listen(port, () => {
